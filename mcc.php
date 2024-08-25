be6903e0f2880053c80bacb8719bd4343146d192
Alexandr <alexandr.arbuzov1503@mail.ru>
2024-08-25 19:24:50 +0500

    first commit

---
+++ mcc.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Cache-Control" content="no-cache">
+        <meta name="description" content="Список MCC кодов у магазинов">
+        <meta property="og:title" content="Список MCC кодов у магазинов">
+        <link rel="stylesheet"
+            href="https://aprogger.ru/access/css/normalize.css">
+        <link rel="shortcut icon"
+            href="https://aprogger.ru/access/img/logo-w.svg"
+            type="image/x-icon" />
+        <link rel="stylesheet" href="style.css">
+        <title>Список MCC кодов у магазинов Сибай, Уфа, Магнитогорск | При
+            поддержке Проекта Свой в Альфа банке</title>
+        <!-- Yandex.RTB -->
+        <script>window.yaContextCb = window.yaContextCb || []</script>
+        <script src="https://yandex.ru/ads/system/context.js" async></script>
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+      (function (m, e, t, r, i, k, a) {
+        m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+        m[i].l = 1 * new Date();
+        for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+      })
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(96505463, "init", {
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+      webvisor: true
+    });
+  </script>
+        <noscript>
+            <div><img src="https://mc.yandex.ru/watch/96505463"
+                    style="position:absolute; left:-9999px;" alt /></div>
+        </noscript>
+        <!-- /Yandex.Metrika counter -->
+    </head>
+
+    <body>
+        <!-- Yandex.RTB R-A-6902447-2 -->
+        <div style="max-width:100%; height:170px;"
+            id="yandex_rtb_R-A-6902447-2"></div>
+        <script>
+    window.yaContextCb.push(() => {
+      Ya.Context.AdvManager.render({
+        "blockId": "R-A-6902447-2",
+        "renderTo": "yandex_rtb_R-A-6902447-2"
+      })
+    })
+  </script>
+        <!-- Yandex.RTB R-A-6902447-1 -->
+        <script>
+    window.yaContextCb.push(() => {
+      Ya.Context.AdvManager.render({
+        "blockId": "R-A-6902447-1",
+        "type": "floorAd",
+        "platform": "desktop"
+      })
+    })
+  </script>
+        <div class="container">
+            <div class="flex center"><a class="header__link" href="#"> <img
+                        class="header__logo"
+                        src="https://aprogger.ru/access/img/Frame 4.svg"
+                        alt="logo"></a><span class="loader"></span></div>
+
+            <h2 class="flex headers">город Сибай</h2>
+
+            <div class="card-container">
+                <div class="card">
+                    <h2>TTS Аструм бутик №9</h2>
+                    <p class="mcc">Маркетплейсы MCC 5399</p>
+                    <p>Адрес: Сибай</p>
+                </div>
+                <div class="card">
+                    <h2>Пиццерия</h2>
+                    <p class="mcc">Кафе и рестораны MCC 5812</p>
+                    <p>Адрес: Сибай </p>
+                </div>
+
+                <div class="card">
+                    <h2>Пятёрочка</h2>
+                    <p class="mcc">Продукты MCC 5411</p>
+                    <p>Адрес: Сибай</p>
+                </div>
+                <div class="card">
+                    <h2>Ермолино</h2>
+                    <p class="mcc">Продукты MCC 5499</p>
+                    <p>Адрес: Сибай </p>
+                </div>
+                <div class="card">
+                    <h2>Суши-хан</h2>
+                    <p class="mcc">Кафе и рестораны MCC 5812</p>
+                    <p>Адрес: Сибай </p>
+                </div>
+                <div class="card">
+                    <h2>СП Блиннок</h2>
+                    <p class="mcc">Фастфуд MCC 5814</p>
+                    <p>Адрес: Сибай, Улица Заки Валиди (Аструм) </p>
+                </div>
+                <div class="card">
+                    <h2>Шаурма</h2>
+                    <p class="mcc">Фастфуд MCC 5814</p>
+                    <p>Адрес: Сибай</p>
+                </div>
+
+                <div class="card">
+                    <h2>Бурёнка</h2>
+                    <p class="mcc">Продукты MCC 5411</p>
+                    <p>Адрес: Сибай</p>
+                </div>
+
+                <div class="card">
+                    <h2>Магнит косметик</h2>
+                    <p class="mcc">Продукты MCC 5411</p>
+                    <p>Адрес: Сибай</p>
+                </div>
+
+                <div class="card">
+                    <h2>Монетка</h2>
+                    <p class="mcc">Продукты MCC 5411</p>
+                    <p>Адрес: Сибай </p>
+                </div>
+
+                <div class="card">
+                    <h2>DNS</h2>
+                    <p class="mcc">Техника MCC 5722</p>
+                    <p>Адрес: Сибай, Улица Горняков 12 </p>
+                </div>
+
+                <div class="card">
+                    <h2>Ветеринарная аптека</h2>
+                    <p class="mcc">Животные MCC 742</p>
+                    <p>Адрес: Сибай, Улица Чайковского, 2 </p>
+                </div>
+
+                <div class="card">
+                    <h2>Сибирское здоровье ИП Semenihina</h2>
+                    <p class="mcc">Аптеки MCC 5912</p>
+                    <p>Адрес: Сибай, Улица Горняков 12 </p>
+                </div>
+
+                <div class="card">
+                    <h2>Кафе Агидель</h2>
+                    <p class="mcc">Фастфуд MCC 5814</p>
+                    <p>Адрес: Сибай, Улица Ленина 17 </p>
+                </div>
+
+                <div class="card">
+                    <h2>ИП Аксеньтев В.А.</h2>
+                    <p class="mcc">Дом и ремонт MCC 5200</p>
+                    <p>Адрес: Сибай, Улица </p>
+                </div>
+
+                <div class="card">
+                    <h2>Центр Современной медицины</h2>
+                    <p class="mcc">Здоровье MCC 8062</p>
+                    <p>Адрес: Сибай, Улица Белова, 19 </p>
+                </div>
+
+                <div class="card">
+                    <h2>Вкусная жизнь</h2>
+                    <p class="mcc">Продукты MCC 5411</p>
+                    <p>Адрес: Сибай, Улица Ленина, 20 </p>
+                </div>
+
+                <div class="card">
+                    <h2>MEDIALAB</h2>
+                    <p class="mcc">Здоровье MCC 8062</p>
+                    <p>Адрес: Сибай </p>
+                </div>
+
+                <div class="card">
+                    <h2>Магнит</h2>
+                    <p class="mcc">Продукты MCC 5411</p>
+                    <p>Адрес:Россия </p>
+                </div>
+
+                <div class="card">
+                    <h2>Siberian Wellness</h2>
+                    <p class="mcc">Продукты MCC 5499</p>
+                    <p>Адрес: Россия (оплата через сайт) </p>
+                </div>
+
+                <div class="card">
+                    <h2>РЖД</h2>
+                    <p class="mcc">Транспорт MCC 4112</p>
+                    <p>Адрес: Россия</p>
+                </div>
+
+                <div class="card">
+                    <h2>Fix Price</h2>
+                    <p class="mcc">Продукты MCC 5331</p>
+                    <p>Адрес:Россия </p>
+                </div>
+
+                <div class="card">
+                    <h2>Медицинские услуги Инвитро</h2>
+                    <p class="mcc">Здоровье MCC 8011</p>
+                    <p>Адрес:Россия</p>
+                </div>
+
+                <div class="card">
+                    <h2>DIXON</h2>
+                    <p class="mcc">Техника MCC 5045</p>
+                    <p>Адрес: г.Сибай, ул.Горняков 11</p>
+                </div>
+
+                <div class="card">
+                    <h2>KL. KHUDAJGULOVA I KO</h2>
+                    <p class="mcc">Здоровье MCC 8099</p>
+                    <p>Адрес: г.Сибай, ул. Чайковского 28</p>
+                </div>
+
+                <div class="card">
+                    <h2>Кафе Азалия</h2>
+                    <p class="mcc">Продукты MCC 5499</p>
+                    <p>Адрес: г.Сибай, ул.Маяковского 27</p>
+                </div>
+
+                <div class="card">
+                    <h2>Адмирал магазин</h2>
+                    <p class="mcc">Продукты MCC 5411</p>
+                    <p>Адрес: г.Сибай,ул. Клары Цеткин, 18</p>
+                </div>
+
+                <div class="card">
+                    <h2>Магазин велосипедов HIFORWARDS</h2>
+                    <p class="mcc">Продукты MCC 5411</p>
+                    <p>Адрес: г.Сибай (площадь)</p>
+                </div>
+
+            </div>
+
+            <h2 class="flex headers">город Уфа</h2>
+
+            <div class="card-container">
+                <div class="card">
+                    <h2>Крендель</h2>
+                    <p class="mcc">Кафе и рестораны MCC 5811</p>
+                    <p>Адрес: Уфа </p>
+                </div>
+                <div class="card">
+                    <h2>Лисичкин хлеб</h2>
+                    <p class="mcc">Фастфуд MCC 5814</p>
+                    <p>Адрес: Уфа </p>
+                </div>
+
+                <div class="card">
+                    <h2>Дворик</h2>
+                    <p class="mcc">Продукты MCC 5411</p>
+                    <p>Адрес: Уфа </p>
+                </div>
+                <div class="card">
+                    <h2>Байрам</h2>
+                    <p class="mcc">Продукты MCC 5411</p>
+                    <p>Адрес: Уфа </p>
+                </div>
+                <div class="card">
+                    <h2>Галамарт</h2>
+                    <p class="mcc">Дом и ремонт MCC 5200</p>
+                    <p>Адрес:</p>
+                </div>
+
+                <div class="card">
+                    <h2>Промедицина</h2>
+                    <p class="mcc">Медицинские услуги MCC 8062</p>
+                    <p>Адрес: г.Уфа </p>
+                </div>
+
+                <div class="card">
+                    <h2>Алексеевский</h2>
+                    <p class="mcc">Продукты MCC 5499</p>
+                    <p>Адрес: г.Уфа </p>
+                </div>
+
+                <div class="card">
+                    <h2>Медицинские анализы KDL</h2>
+                    <p class="mcc">Здоровье MCC 8071</p>
+                    <p>Адрес: г.Уфа Степана Кувыкина, 18 1 этаж</p>
+                </div>
+
+                <div class="card">
+                    <h2>SHAVERMA ORIGINAL</h2>
+                    <p class="mcc">Кафе и рестораны MCC 5812</p>
+                    <p>Адрес: г.Уфа,Мубарякова, 13Б </p>
+                </div>
+                <div class="card">
+                    <h2>CENTR MOD</h2>
+                    <p class="mcc">Одежда и обувь MCC 5611</p>
+                    <p>Адрес: г.Уфа, ​Рабкоров, 20 ​цокольный этаж </p>
+                </div>
+
+            </div>
+
+        </div>
+
+        <!-- Yandex.RTB R-A-6902447-2 -->
+        <div style="max-width:100%; height:170px;"
+            id="yandex_rtb_R-A-6902447-2"></div>
+        <script>
+window.yaContextCb.push(() => {
+ Ya.Context.AdvManager.render({
+   "blockId": "R-A-6902447-2",
+   "renderTo": "yandex_rtb_R-A-6902447-2"
+ })
+})
+</script>
+    </body>
+
+</html>
